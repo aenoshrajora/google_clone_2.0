71e8f801e85bbf909b628a0836f0e0f3df54fe68
Aenosh Rajora <aenoshrajora79@gmail.com>
2022-06-24 13:28:42 +0530

    Initial Commit
    Live site:- https://googl2.netlify.app

---
+++ src/contexts/StateContextProvider.js
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+        'X-User-Agent': 'desktop',
+        'X-RapidAPI-Key': '23f8328379mshbe85da01733760dp1f6647jsne7ea38645dca',
+        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
